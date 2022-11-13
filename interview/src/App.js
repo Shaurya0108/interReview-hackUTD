1aa0302edb81d83a606a2e022e258946d82814a3
Buchimi <stormzzy900@gmail.com>
2022-11-12 23:25:29 -0600

    Video record lite

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { useRecordWebcam } from 'react-record-webcam'
+import { useRecordWebcam, WebcamStatus } from 'react-record-webcam'
+import { useState } from 'react';
+import "./Buttons"
+import RenderButtons from './Buttons';
 
 function RecordVideo(props) {
   const recordWebcam = useRecordWebcam({ frameRate: 60 });
@@ -8,18 +11,41 @@ function RecordVideo(props) {
   const saveFile = async () => {
     const blob = await recordWebcam.getRecording();
   };
+  const [recording, setrecording] = useState(true)
+  const [firstStart, setfirststart] = useState(true)
+  function onRecord() {
 
+    recordWebcam.start()
+
+
+  }
+
+  function onRecordStop() {
+    setrecording(false)
+    recordWebcam.stop()
+  }
+
+  function onRecordRetake() {
+    setrecording(true)
+    recordWebcam.retake()
+  }
+  //recordWebcam.open()
   return (
     <div>
       <p>Camera status: {recordWebcam.status}</p>
-      <button onClick={recordWebcam.open}>Open camera</button>
-      <button onClick={recordWebcam.start}>Start recording</button>
-      <button onClick={recordWebcam.stop}>Stop recording</button>
-      <button onClick={recordWebcam.retake}>Retake recording</button>
+
+      {/* <button onClick={recordWebcam.open}>Open camera</button>
+      <button onClick={onRecord}>Start recording</button>
+      <button onClick={onRecordStop}>Stop recording</button>
+      <button onClick={onRecordRetake}>Retake recording</button>
       <button onClick={recordWebcam.download}>Download recording</button>
-      <button onClick={saveFile}>Save file to server</button>
-      <video ref={recordWebcam.webcamRef} autoPlay muted />
-      <video ref={recordWebcam.previewRef} autoPlay muted loop />
+      <button onClick={saveFile}>Save file to server</button> */}
+      <RenderButtons recordWebcam={recordWebcam} setrecording={setrecording} saveFile={saveFile} />
+      <h3> Why do you want to work at apple? </h3>
+
+      {recording && <video ref={recordWebcam.webcamRef} autoPlay muted />}
+      {!recording && <video ref={recordWebcam.previewRef} autoPlay muted loop />}
+
     </div>
   )
 }
