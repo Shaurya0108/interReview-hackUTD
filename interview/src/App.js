82d29dbddccb4c891376a4c63e1b8af679829178
Buchimi <stormzzy900@gmail.com>
2022-11-12 21:56:34 -0600

    video init

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRecordWebcam } from 'react-record-webcam'
 
-function App() {
+function RecordVideo(props) {
+  const recordWebcam = useRecordWebcam({ frameRate: 60 });
+
+  const saveFile = async () => {
+    const blob = await recordWebcam.getRecording();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Camera status: {recordWebcam.status}</p>
+      <button onClick={recordWebcam.open}>Open camera</button>
+      <button onClick={recordWebcam.start}>Start recording</button>
+      <button onClick={recordWebcam.stop}>Stop recording</button>
+      <button onClick={recordWebcam.retake}>Retake recording</button>
+      <button onClick={recordWebcam.download}>Download recording</button>
+      <button onClick={saveFile}>Save file to server</button>
+      <video ref={recordWebcam.webcamRef} autoPlay muted />
+      <video ref={recordWebcam.previewRef} autoPlay muted loop />
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default RecordVideo;
